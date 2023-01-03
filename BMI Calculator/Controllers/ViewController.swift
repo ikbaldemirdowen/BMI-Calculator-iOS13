b638ef3460e4f56a61b452d38dd31ed5da8302f3
Ikbal Demirdoven <ikbaldemirdoven@icloud.com>
2023-01-02 23:04:23 -0800

    changed BMI calculation to make it more precise.

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -15,6 +15,31 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
+    var heightt : Int = 0
+    var weightt : Int = 0
+    
+    @IBAction func heightSlider(_ sender: UISlider)
+    {
+        let height = String(format: "%.2f", sender.value)
+        heightLabel.text = "\(height)m"
+    }
+    
+    
+    @IBAction func weightSlider(_ sender: UISlider)
+    {
+        let weight = String(format: "%.0f", sender.value)
+        weightLabel.text = "\(weight)kg"
+    }
+    @IBAction func calculatePressed(_ sender: UIButton)
+    {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        let BMI = weight / (height * height)
+        print(BMI)
+    }
 }
-
